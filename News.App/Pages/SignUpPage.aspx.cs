99455d7257d36b5f06c96fb0a0e64287205741ce
Mohammadshakouri <shakouri1380@gmail.com>
2023-03-13 21:13:42 +0330

    2

--- News.App/Pages/SignUpPage.aspx.cs
+++ News.App/Pages/SignUpPage.aspx.cs
@@ -17,12 +17,8 @@ namespace News.App.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            }
-            else
-            {
-                string role = GetUserRole();
-                useridentity.InnerHtml = $"<i class=\"bi-file-person-fill\" style=\"margin-left:5px; font-size:1.25rem; vertical-align: middle;\"></i>{HttpContext.Current.User.Identity.Name} ({role})";
-                header.Visible = true;
+        }
+
 
         protected void BTnSignUp_Click(object sender, EventArgs e)
         {
@@ -38,25 +34,6 @@ namespace News.App.Pages
             Response.Redirect("LoginPage.aspx?signupcondition=success");
         }
 
-                switch (userroleFromDB)
-                {
-                    case "Admin": UserRole = "مدیر کل سیستم"; break;
-                    case "Author": UserRole = "نویسنده خبر"; break;
-                    case "Editor": UserRole = "ویرایشگر"; break;
-                    case "Member": UserRole = "کاربر"; break;
-                }
-            }
-            return UserRole;
-        }
-
-            }
-
-            if (UserRoleInDb != "Admin")
-            {
-                System.Web.Security.FormsAuthentication.SignOut();
-                System.Web.Security.FormsAuthentication.RedirectToLoginPage();
-            }
-
-        }
     }
+    
 }
